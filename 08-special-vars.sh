91d7c6a71bb816946c60ee62a9a19248e127c9e0
sathish reddy <sathishmedipally22@gmail.com>
2025-01-06 17:18:57 +0530

    shell -scripting

---
+++ 08-special-vars.sh
@@ -0,0 +1,16 @@
+echo "all variables are passed: $@"
+
+echo "number of variables: $#"
+
+echo "script name: $0"
+
+echo "present working directory: $PWD"
+
+echo "home directory of current user: $HOME"
+
+echo "which user is running the script: $USER"
+
+echo "process id of current script: $$"
+
+sleep 60 &
+echo "process id of last command in background: $!"
