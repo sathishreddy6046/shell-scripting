8a32cc164710f216a33ab1606e29b65ba91ce342
sathish reddy <sathishmedipally22@gmail.com>
2025-01-06 15:58:12 +0530

    shell -scripting

---
+++ 07-arrays.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MOVIES=("ssmb29""pokiri""athadu")
+
+echo "first movie : ${MOVIES[0]}"
+
+
+echo "first movie : ${MOVIES[0]}"
+
+echo "second movie : ${MOVIES[1]}"
+
+echo "third movie : ${MOVIES[2]}"
+
+echo "All movies are : ${MOVIES[@]}"
