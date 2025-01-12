f1922f3166a2f9e2cbfd62e557f246cf43a6b8eb
sathish reddy <sathishmedipally22@gmail.com>
2025-01-13 00:07:24 +0530

    shell-scripting

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -10,6 +10,9 @@ then
 
  fi
 
+ dnf list installed mysql
+ if [ $? -ne 0 ]
+ then
  dnf install mysql -y
 
  if [ $? -ne 0 ]
@@ -19,8 +22,44 @@ then
 
     else
      echo : "installing mysql is success"
+
      fi
+
+     else
+      echo : "installing mysql is installed"
+      fi
+
+
+ dnf list installed git
+  if [ $? -ne 0 ]
+  then
  dnf install git -y
+if [ $? -ne 0 ]
+then
+   echo : "installing git is failure"
+   exit 1
+
+else
+    echo : "installing git is success"
+
+     fi
+     else
+     echo : "installing git is installed"
+      fi
+
+
+
+#  dnf install mysql -y
+
+#  if [ $? -ne 0 ]
+#   then
+#    echo : "installing mysql is failure"
+#    exit 1
+
+#     else
+#      echo : "installing mysql is success"
+#      fi
+#  dnf install git -y
 
 if [ $? -ne 0 ]
 then
